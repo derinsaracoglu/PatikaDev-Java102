f3c09ba88e998bc9e1ab5ca11a6189a28e17a4e8
Derin Saracoglu <64663540+derinsaracoglu@users.noreply.github.com>
2022-06-27 13:16:25 +0300

    Update and rename Ödev1 to Ödev1.sql

--- SQL/Ödev1.sql
+++ SQL/Ödev1.sql
@@ -12,3 +12,5 @@ SELECT first_name,last_name FROM customer WHERE first_name = 'Mary'   CEVAP: SMI
 
 --film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
 SELECT * FROM film WHERE length>50 AND NOT (rental_rate = 2.99 OR rental_rate = 4.99);
+
+
