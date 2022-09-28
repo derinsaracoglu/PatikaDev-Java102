eda6bfd2eb62e2e30762555d82d6df174b1b764d
Derin Saracoglu <64663540+derinsaracoglu@users.noreply.github.com>
2022-09-28 15:51:12 +0300

    Add files via upload

---
+++ Java102/MultiThread/Thread/Thread.java
@@ -0,0 +1,28 @@
+package Thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Thread {
+    public static void main(String[] args) {
+        ThreadActions homeWork = new ThreadActions();
+
+        ExecutorService executor = Executors.newFixedThreadPool(15);
+
+        for (int i = 0; i < 10000; i++) {
+            executor.execute(homeWork);
+
+        }
+
+        executor.shutdown();
+        try {
+            executor.awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Even Numbers : " + homeWork.getEvenNumber().size());
+
+    }
+}
